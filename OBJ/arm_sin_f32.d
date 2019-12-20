787ddb84b38f001f551a611ec3be99b143d9f5b1
wyd-David <34279650+wyd-David@users.noreply.github.com>
2019-12-20 14:47:25 +0800

    Add files via upload

---
+++ OBJ/arm_sin_f32.d
@@ -0,0 +1,9 @@
+..\obj\arm_sin_f32.o: ..\Libraries\src\arm_sin_f32.c
+..\obj\arm_sin_f32.o: ..\Libraries\inc\arm_math.h
+..\obj\arm_sin_f32.o: ..\Libraries\inc\core_cm4.h
+..\obj\arm_sin_f32.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\arm_sin_f32.o: ..\USER\core_cmInstr.h
+..\obj\arm_sin_f32.o: ..\USER\core_cmFunc.h
+..\obj\arm_sin_f32.o: ..\USER\core_cm4_simd.h
+..\obj\arm_sin_f32.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+..\obj\arm_sin_f32.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
